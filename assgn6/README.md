7f5e974a7615104a92af37fe9aa51eb1ee53c0ab
Rissa <rissajiyeon.lee@gmail.com>
2019-10-14 09:52:44 -0400

    assignment6

---
+++ assgn6/README.md
@@ -0,0 +1,14 @@
+# rissajlee.github.io
+
+The link to the website is: https://rissajlee.github.io/homework_5/index.html
+
+This project is best viewed in a 1024x900 resolution. 
+This is due to the original designs having been created for that generic Desktop size in the Sketch app.
+
+The three pages of interest for this homework assignment are 'index.html', 'shop.html', and 'original.html', which correlate to the homepage, shop page, and original bun bun page respectively.
+
+All iconography and code that was found from outside resources is noted in the .html and .css files. They will be noted by attribution comments.
+
+In addition, all other image attributions (mainly on the PLP) are cited in the file 'imageAttributions.png'.
+
+Thank you!
--- week7_lab/function.js
+++ week7_lab/function.js
@@ -1,1 +1,55 @@
+function whale(name, age) {
+    this.name = name;
+    this.age = age;
+    this.type = "whale";
+    this.image ="whale.png";
+}
 
+function sheep(name,age) {
+    this.name = name;
+    this.age = age;
+    this.type = "sheep";
+    this.image ="sheep.png";
+}
+
+function chicken(name, age) {
+    this.name = name;
+    this.age = age;
+    this.type = "chicken";
+    this.image ="chicken.png";
+}
+
+var animal = [new whale("whaley", 5), new whale("mary",1), new whale("run", 2)];
+
+var animal2 = [new whale("junior", 2), new sheep("blue", 3), new chicken("gary",80)];
+
+var generateRandomIndex = function (maxIndex) {
+    var random = Math.floor(Math.random()*maxIndex);
+    console.log(random);
+}
+
+var generateRandomName = function (animal) {
+    var randomIndex = generateRandomIndex(animal.length);
+    var randomAnimal = (animal[randomIndex]).name;
+    console.log(randomAnimal);
+    // return randomAnimal.name;
+}
+
+var generateRandomAge = function (animal) {
+    var randomIndex = generateRandomIndex(animal.length);
+    console.log((animal[randomIndex]).age);
+}
+
+var generateRandomAnimal = function (animals) {
+    var randomIdx = generateRandomIndex(animals.length);
+    var randomAnimal = animal[randomIdx];
+    if (randomAnimal instanceof "whale") {
+        return new whale(generateRandomName(animals.length), generateRandomAge(animals.length));
+    }
+    if (randomAnimal instanceof "sheep") {
+        return new sheep(generateRandomName(animals.length), generateRandomAge(animals.length));
+    }
+    if (randomAnimal instanceof "chicken") {
+        return new chicken(generateRandomName(animals.length), generateRandomAge(animals.length));
+    }
+}
