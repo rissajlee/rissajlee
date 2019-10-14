db3b107cedad88567b443e67b2bcb054d5459d09
Rissa <rissajiyeon.lee@gmail.com>
2019-10-14 10:36:32 -0400

    JS Updates

--- assgn6/function.js
+++ assgn6/function.js
@@ -1,1 +1,31 @@
-f
+
+
+document.getElementById("none").onclick = function() {
+    document.getElementById("none").style.color = "orange";
+    document.getElementById("sugarmilk").style.color = "black";
+    document.getElementById("vanillamilk").style.color = "black";
+    document.getElementById("doublechoc").style.color = "black";
+
+}
+
+document.getElementById("sugarmilk").onclick = function() {
+    document.getElementById("sugarmilk").style.color = "orange";
+    document.getElementById("none").style.color = "black";
+    document.getElementById("vanillamilk").style.color = "black";
+    document.getElementById("doublechoc").style.color = "black";
+}
+
+document.getElementById("vanillamilk").onclick = function() {
+    document.getElementById("sugarmilk").style.color = "black";
+    document.getElementById("none").style.color = "black";
+    document.getElementById("vanillamilk").style.color = "orange";
+    document.getElementById("doublechoc").style.color = "black";
+
+}
+
+document.getElementById("doublechoc").onclick = function() {
+    document.getElementById("sugarmilk").style.color = "black";
+    document.getElementById("none").style.color = "black";
+    document.getElementById("vanillamilk").style.color = "black";
+    document.getElementById("doublechoc").style.color = "orange";
+}
