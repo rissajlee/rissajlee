d365866ca8f5cb9c0e3ca3805b47c8d6a0bf902c
Rissa <rissajiyeon.lee@gmail.com>
2019-10-30 22:48:54 -0400

    This commit is the final update to the assignment functionality

--- assgn6B/function.js
+++ assgn6B/function.js
@@ -40,7 +40,6 @@ function Bun (quantity, glaze, price) {
     this.price = price;
 }
 
-
 // Updates price in PDP
 function updatePDPTotal (quantity) {
     document.getElementById("originalPDPtotal").innerHTML = "Total&emsp;&emsp;$" + (quantity*3);
@@ -73,12 +72,9 @@ function addToCart() {
     var radioNumber = document.querySelector('input[name=numberOfBuns]:checked').value;
     var newBun = new Bun(parseInt(radioNumber), checkGlaze(), parseInt(radioNumber)*3);
 
-
+    // adds new bun to existing cart array
     var test = JSON.parse(sessionStorage.getItem("shoppingCart"));
-
     test.push([newBun]);
-
-    console.log(test);
     sessionStorage.setItem("shoppingCart", JSON.stringify(test));
 
     // updates cart total
@@ -90,11 +86,12 @@ function addToCart() {
     }
 }
 
-
+// creates grid of items on Cart page 
 function showCart() {
     var cart = JSON.parse(sessionStorage.getItem("shoppingCart"));
     var cartGrid = document.getElementById("cartGrid");
     // Referenced https://www.w3schools.com/js/js_loop_for.asp
+    // goes through cart array and creates row in cart for each
     var i = 0;
     for (i = 0; i < cart.length; i++) {
         var bunArray = cart[i];
@@ -107,30 +104,53 @@ function showCart() {
         var newRow = document.createElement('div');
         newRow.setAttribute('class', 'grid');
         newRow.setAttribute('id', i)
+        // Creates cell for number of buns
         var newGrid1 = document.createElement('div');
         newGrid1.setAttribute('class', "grid_item_4");
         newGrid1.innerHTML = quantity;
+        // Creates cell for type of bun
         var newGrid2 = document.createElement('div');
-        newGrid2.setAttribute("class", "grid_item_5");
-        newGrid2.innerHTML = "original bun bun";
+        newGrid2.setAttribute("class", "grid_item_5img");
+        var bunImage = document.createElement('img');
+        bunImage.setAttribute("src", "assets/originalCart.png");
+        bunImage.setAttribute("width", "80px");
+        newGrid2.appendChild(bunImage);
+        // Creates cell linking bun and glaze
         var newGrid3 = document.createElement('div');
         newGrid3.setAttribute("class", "grid_item_4");
         newGrid3.innerHTML = "with glaze";
+        // Creates cell with glaze 
         var newGrid4 = document.createElement('div');
-        newGrid4.innerHTML = glaze;
-        newGrid4.setAttribute("class", "grid_item_5");
+        newGrid4.setAttribute("class", "grid_item_5img");
+        var glazeImage = document.createElement('img');
+        if (glaze == "none") {
+            glazeImage.setAttribute("src", "assets/noneCart.png");
+        } else if (glaze == "sugarmilk") {
+            glazeImage.setAttribute("src", "assets/sugarMilkCart.png");
+        } else if (glaze == "vanillamilk") {
+            glazeImage.setAttribute("src", "assets/vanillaMilkCart.png");
+        } else {
+            glazeImage.setAttribute("src", "assets/doubleChocCart.png");
+        }
+        glazeImage.setAttribute("width", "80px");
+        newGrid4.appendChild(glazeImage);
+        // Spacer cell
         var newGrid5 = document.createElement('div');
         newGrid5.setAttribute("class", "grid_item_4");
+        // Creates cell with text "total"
         var newGrid6 = document.createElement('div');
         newGrid6.setAttribute("class", "grid_item_5");
         newGrid6.innerHTML = "Total:";
+        // Creates cell listing price
         var newGrid7 = document.createElement('div');
         newGrid7.setAttribute("class", "grid_item_4");
         newGrid7.innerHTML = "$" + price;
+        // Creates cell with option to delete
         var newGrid8 = document.createElement('div');
         newGrid8.setAttribute("class", "grid_item_6");
         newGrid8.setAttribute("onclick", "deleteFromCart(this)");
-        newGrid8.innerHTML = "x";
+        newGrid8.innerHTML = "delete";
+        // Adds all cells to row, then to the entire grid
         newRow.appendChild(newGrid1);
         newRow.appendChild(newGrid2);
         newRow.appendChild(newGrid3);
@@ -144,37 +164,31 @@ function showCart() {
 }
 
 
-
+// deletes buns from cart
 function deleteFromCart(deleteX) {
 
     var parentRow = deleteX.parentNode;
-    
-    console.log(oldPrice);
     var index = parseInt(parentRow.id);
-
     var cart = JSON.parse(sessionStorage.getItem("shoppingCart"));
-
+    // updates total price on cart page and stored variable
     var oldPrice = parseInt(cart[index][0].price);
     var newPrice = parseInt(sessionStorage.getItem("cartTotal")) - oldPrice;
     sessionStorage.setItem("cartTotal", JSON.stringify(newPrice));
     var printPrice = sessionStorage.getItem("cartTotal")
     document.getElementById("cartPageTotal").innerHTML = "$&emsp;" + newPrice;
 
+    // removes deleted item from cart array and updates
     cart.splice(index,1);
-
     sessionStorage.setItem("shoppingCart", JSON.stringify(cart));
 
+    // removes deleted item from cart page
     var grid = parentRow.parentNode;
-
     var gridContainer = grid.parentNode;
-
     gridContainer.removeChild(grid);
-
+    // draws new grid without deleted item
     var newGrid = document.createElement('div');
     newGrid.setAttribute("id","cartGrid");
-
     gridContainer.insertBefore(newGrid, gridContainer.childNodes[0]);
-
     showCart();
 
     // updates cart icon counter
@@ -182,8 +196,6 @@ function deleteFromCart(deleteX) {
     var newTotal = oldTotal - 1;
     sessionStorage.setItem("total", newTotal);
     document.getElementById("cartValue").innerHTML = "(" + newTotal + ")";
-
-
 }
 
 
@@ -215,6 +227,4 @@ function onLoad() {
     if (cCart.length != 0) {
         showCart();
     }
-
-
 }
